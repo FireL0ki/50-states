80be4b643344211802078137f66e224bb4f5c8e7
muell <muell758@d.umn.edu>
2021-12-09 15:33:17 -0600

    videos 1-6

---
+++ client/src/components/StateList.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <!-- v-bind:key binds is a value that is unique for each individual object -->
+        <p v-for="state in states" v-bind:key="state.name"> 
+            {{ state.name }}
+        </p>
+    </div>
+</template>
+
+
+<script>
+
+export default {
+    name: 'StateList',
+    data() {
+        return {
+            states: []
+        }
+    },
+    mounted() {
+        this.fetchAllStates()
+    },
+    methods: {
+        fetchAllStates() {
+            this.$stateService.getAllStates().then ( states => {
+                this.states = states
+            })
+        }
+    }
+}
+</script>
+
+
+<style scoped>
+
+</style>
---
+++ client/src/main.js
@@ -0,0 +1,14 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import stateAPIService from '@/services/stateService'
+
+import router from '@/router'
+
+let app = createApp(App)
+
+app.config.globalProperties.$stateService = stateAPIService 
+
+app.use(router)
+
+app.mount('#app')
